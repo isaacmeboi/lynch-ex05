af2fa302cbd90d41d65f03af6832606af1151e12
isaac lynch <thebunnyarcher@gmail.com>
2021-09-03 13:35:20 -0400

    forgot the psuedocode again at the bottom, as well as formatting the code so it isnt a BLOCK.

--- src/main/java/exercise05/Solution05.java
+++ src/main/java/exercise05/Solution05.java
@@ -13,9 +13,19 @@ public class Solution05 {
             System.out.println("Here are you pathetic solutions.");
 
             System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
+
             System.out.println(n1 + " - " + n2 + " = " + (n1-n2));
+
             System.out.println(n1 + " * " + n2 + " = " + (n1*n2));
+
             System.out.println(n1 + " / " + n2 + " = " + (n1/n2));
 
         }
     }
+    /*the textbook also helped with formatting this solution. first, we import
+    the java.util library to use the scanner function. after that a main function
+    is created and an instance and declaration of the scanner function is made.
+    then, the first prompt is printed and then the next input is read by the
+    computer. a second prompt is given with another scanner function to read
+    the number input by the user. finally, all the solutions are printed in
+    a list using the println function and built in means to do mathematics.*/
